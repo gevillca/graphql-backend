baaf6c91f6f9b3c923fb91c87f1dc481c11915bf
gevillca <gevillca@gmail.com>
2021-10-24 20:27:08 -0400

    new changes

--- build/context.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createContext = void 0;
-const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
-function createContext(request) {
-    return { request, prisma };
-}
-exports.createContext = createContext;
